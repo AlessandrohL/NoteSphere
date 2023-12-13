7e5dda9f23641e9d501e2f0634115e556455bfa0
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-12 20:46:20 -0500

    Add Service extension

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            string connection = configuration.GetConnectionString("DevConnection")
+                ?? throw new InvalidOperationException("Connection string is null.");
+
+            // Configure DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connection));
+
+            // Configure Identity
+            services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredLength = 8;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+
+            return services;
+        }
+    }
+}
