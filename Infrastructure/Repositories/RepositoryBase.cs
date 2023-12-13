bd543f70737f9f3f1e67c0094139370d122e9219
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-12 21:36:24 -0500

    Implementation of the Generic Repository

---
+++ Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Abstractions.Repositories;
+using Domain.Common;
+using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public abstract class RepositoryBase<TEntity, TKey> 
+        : IRepositoryBase<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public RepositoryBase(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException($"{nameof(context)} is null");
+        }
+
+
+        public IQueryable<TEntity> FindAll(bool trackChanges) => !trackChanges
+            ? _context.Set<TEntity>().AsNoTracking()
+            : _context.Set<TEntity>().AsTracking();
+
+        public IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool trackChanges)
+            => !trackChanges
+            ? _context.Set<TEntity>().AsNoTracking()
+            : _context.Set<TEntity>().AsTracking();
+
+        //public async Task<TEntity> FindById(TKey id)
+        //{
+        //    return await _context.Set<TEntity>().FindAsync(id);
+        //}
+
+        public void Create(TEntity entity) => _context.Set<TEntity>().Add(entity);
+
+        public void Delete(TEntity entity) => _context.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity) => _context.Set<TEntity>().Update(entity);
+    }
+}
