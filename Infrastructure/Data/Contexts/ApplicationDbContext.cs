51c622ca5647ca36806c22d5fbcdcbf476d32689
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-12 20:43:55 -0500

    Add DbContext

---
+++ Infrastructure/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<NoteBook> NoteBooks { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<NoteTag> NoteTags { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
