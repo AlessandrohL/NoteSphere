c6d1043c163cf2e2a8d16f7d5d00493af229fda6
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 13:07:31 -0500

    AutoMapper configuration

---
+++ src/NoteSphere.Application/Models/Mapper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using Application.Models.DTOs.User;
+using Application.Models.Identity;
+using AutoMapper;
+using Domain.Abstractions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserRegistrationDto, UserAuth>();
+            CreateMap<UserRegistrationDto, ApplicationUser>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
