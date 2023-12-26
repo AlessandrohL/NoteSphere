ca98a3f9ed2c223aa6c9b813d388428befd3c2e4
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 13:21:52 -0500

    Configuring authentication with JWT

---
+++ src/NoteSphere.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddAuthenticationWithJWT
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero,
+
+                        ValidIssuer = jwtSettings["ValidIssuer"],
+                        ValidAudience = jwtSettings["ValidAudience"],
+                        IssuerSigningKey = new SymmetricSecurityKey
+                            (Encoding.UTF8.GetBytes(jwtSettings["Secret"]))
+                    };
+                });
+        }
+    }
+}
--- src/NoteSphere.WebApi/Program.cs
+++ src/NoteSphere.WebApi/Program.cs
@@ -1,13 +1,27 @@
+using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+   // options.SuppressModelStateInvalidFilter = true;
+});
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.AddAuthenticationWithJWT(builder.Configuration);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
