9c268b2ccae95d82328d5034c58feca42aacd93f
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 12:59:00 -0500

    New user identity interface

---
+++ src/NoteSphere.Domain/Abstractions/IUserWithIdentityFeatures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface IUserWithIdentityFeatures
+    { 
+        public string Id { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
