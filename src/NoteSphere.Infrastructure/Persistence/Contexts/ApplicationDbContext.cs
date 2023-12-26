6985f136de2d2df3e8697d21a4879e559b318935
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 13:13:37 -0500

    Adding DbContext

---
+++ src/NoteSphere.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Application.Models.Identity;
+using Domain.Entities;
+using Infrastructure.Data.Configurations;
+using Infrastructure.Identity;
+using Infrastructure.Identity.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<UserAuth>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new UserConfiguration());
+
+            builder.ApplyConfiguration(new ApplicationUserConfiguration());
+            builder.ApplyConfiguration(new NoteBookConfiguration());
+            builder.ApplyConfiguration(new NoteConfiguration());
+            builder.ApplyConfiguration(new TagConfiguration());
+            builder.ApplyConfiguration(new NoteTagConfiguration());
+            builder.ApplyConfiguration(new TodoConfiguration());
+        }
+
+        public DbSet<NoteBook> NoteBooks { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<NoteTag> NoteTags { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
