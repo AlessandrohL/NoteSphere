49966620e96e20db0faee1a13ee9ece0a4dcb976
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 13:07:13 -0500

    Identity User

---
+++ src/NoteSphere.Application/Models/Identity/UserAuth.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Identity
+{
+    public class UserAuth : IdentityUser, IUserWithIdentityFeatures
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
