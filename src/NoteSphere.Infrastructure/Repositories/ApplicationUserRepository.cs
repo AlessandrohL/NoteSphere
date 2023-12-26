092f209c2ab46ae9d47bf7a2a194da6266543636
Alessandro Luis <luisalexandrocordova@gmail.com>
2023-12-26 13:17:02 -0500

    Adding ApplicationUserRepository

---
+++ src/NoteSphere.Infrastructure/Repositories/ApplicationUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Abstractions.Repositories;
+using Domain.Entities;
+using Infrastructure.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ApplicationUserRepository : RepositoryBase<ApplicationUser, Guid>, IApplicationUserRepository
+    {
+        public ApplicationUserRepository(ApplicationDbContext context)
+            : base(context)
+        { }
+    }
+}
